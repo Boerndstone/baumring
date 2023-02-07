b045d3bce2f1ba58ec9659e4045700571b4118e9
Bernd Ullmann <berndullmann@Bernds-MacBook-Air.local>
2023-02-07 12:45:32 +0100

    mdb template is working now

--- assets/app.js
+++ assets/app.js
@@ -17,3 +17,30 @@ import "./bootstrap";
 
 import * as mdb from "mdb-ui-kit"; // lib
 window.mdb = mdb;
+
+const sidenav = document.getElementById("sidenav-1");
+const sidenavInstance = mdb.Sidenav.getInstance(sidenav);
+
+let innerWidth = null;
+
+const setMode = (e) => {
+  // Check necessary for Android devices
+  if (window.innerWidth === innerWidth) {
+    return;
+  }
+
+  innerWidth = window.innerWidth;
+
+  if (window.innerWidth < 1400) {
+    sidenavInstance.changeMode("over");
+    sidenavInstance.hide();
+  } else {
+    sidenavInstance.changeMode("side");
+    sidenavInstance.show();
+  }
+};
+
+setMode();
+
+// Event listeners
+window.addEventListener("resize", setMode);
