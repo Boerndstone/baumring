bfb6004831ecb8e1b149e5baeb2e0a894cb2301c
Bernd Ullmann <berndullmann@Bernds-MacBook-Air.local>
2023-05-12 14:45:01 +0200

    karte

--- assets/app.js
+++ assets/app.js
@@ -18,33 +18,6 @@ import "./bootstrap";
 import * as mdb from "mdb-ui-kit"; // lib
 window.mdb = mdb;
 
-const sidenav = document.getElementById("sidenav-1");
-const sidenavInstance = mdb.Sidenav.getInstance(sidenav);
-
-let innerWidth = null;
-
-const setMode = (e) => {
-  // Check necessary for Android devices
-  if (window.innerWidth === innerWidth) {
-    return;
-  }
-
-  innerWidth = window.innerWidth;
-
-  if (window.innerWidth < 1400) {
-    sidenavInstance.changeMode("over");
-    sidenavInstance.hide();
-  } else {
-    sidenavInstance.changeMode("side");
-    sidenavInstance.show();
-  }
-};
-
-setMode();
-
-// Event listeners
-window.addEventListener("resize", setMode);
-
 const imagesContext = require.context(
   "./images",
   true,
